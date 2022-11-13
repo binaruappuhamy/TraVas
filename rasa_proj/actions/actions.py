134f969b4e740fe3b9bcf1b96bcde9b3ed1f9c2a
binaruappuhamy <binaruappuhamy>
2022-11-13 18:04:05 -0500

    Add rasa module

---
+++ rasa_proj/actions/actions.py
@@ -0,0 +1,31 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+import logging
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+# logger = logging.getLogger(__name__)
+
+class ActionTest(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # logger.warning("REACHED HERE !!!!!!!!!!!")
+        print("REACHED HERE!")
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
