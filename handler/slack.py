da72a520060dcee0c7d633ca4f489cce9f728b6b
binaruappuhamy <binaruappuhamy>
2022-11-15 21:42:19 -0500

    Add slack functionality to subscribe to message events on channel

--- handler/slack.py
+++ handler/slack.py
@@ -1,22 +1,25 @@
 import logging
-import json
-import time
 import os
-import slack_sdk
-from dotenv import load_dotenv
-
+from slack_sdk.socket_mode.websocket_client import SocketModeClient
+from slack_sdk.web.async_client import AsyncWebClient
+from slack_sdk.socket_mode.aiohttp import SocketModeClient
+        
 
 class Slack:
     def __init__(self):
-        load_dotenv()
         self.token = os.getenv('SLACK_TOKEN')
+        self.app_token = os.getenv('SLACK_APP_TOKEN')
         self.channelID = os.getenv('SLACK_CHANNEL')
-        self.client = slack_sdk.WebClient(token=self.token)
+        self.client = SocketModeClient(
+                        app_token=self.app_token, 
+                        web_client=AsyncWebClient(token=self.token)
+                    )
 
     def Print(self):
         print(self.token)
         print(self.channelID)
 
+    #function not needed anymore
     def GetMessages(self):
         result = self.client.conversations_history(
             channel=self.channelID, oldest="1668389600")
@@ -38,22 +41,26 @@ class Slack:
         try:
             processed_text = None
             if "text" in msg:
-                try:
-                    # processed_text = msg["blocks"][0]["elements"][0]["elements"][1]["text"]
-                    block_list = msg["blocks"]
-                    for block in block_list:
-                        if block["type"] == "rich_text":
-                            element_list = block["elements"]
-                            for element in element_list:
-                                if element["type"] == "rich_text_section":
-                                    text_element_list = element["elements"]
-                                    for text_el in text_element_list:
-                                        if text_el["type"] == "text":
-                                            processed_text = text_el["text"]
-                except KeyError:
-                    pass
-            
-        except:
-            logging.exception("Unhandled Error")
+                block_list = msg["blocks"]
+                for block in block_list:
+                    if block["type"] == "rich_text":
+                        element_list = block["elements"]
+                        for element in element_list:
+                            if element["type"] == "rich_text_section":
+                                text_element_list = element["elements"]
+                                for text_el in text_element_list:
+                                    if text_el["type"] == "text":
+                                        processed_text = text_el["text"]   
+        except Exception as e:
+            logging.exception(str(repr(e)))
         else:
             return processed_text
+
+    def post_message(self, msg):
+        try:
+            self.client.web_client.chat_postMessage(
+                channel=os.getenv('SLACK_CHANNEL'),
+                text=msg
+            )
+        except Exception as e:
+            logging.exception(str(repr(e)))
